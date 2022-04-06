e3d62b554c99c09d6ad173591501e4f3afef3f69
cyy <31358284+cyyloading@users.noreply.github.com>
2022-04-06 00:28:19 -0500

    Create OddOccurrencesInArray.py

---
+++ Lesson 2 - Arrays/OddOccurrencesInArray.py
@@ -0,0 +1,15 @@
+def solution(A):
+    B = {}  #create a empty dict to storage the distinct values in A and their frequency
+    for i in A:
+        if i not in B:
+            B[i] = 1
+        else:
+            B[i] += 1
+    
+    for i in B: 
+        if B[i]%2 != 0:  # check if the frequency of the value is not even times
+            return i
+    pass
+  
+print(solution([9,3,9,3,9,7,9]))
+print(solution([9,3,9,3,9,7,7]))
