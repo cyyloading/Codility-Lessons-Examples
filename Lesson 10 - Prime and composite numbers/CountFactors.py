18c8c15ec4c915ad9f8d3de22eb55e9d9a0926a7
cyy <31358284+cyyloading@users.noreply.github.com>
2022-04-17 13:02:13 -0500

    Create CountFactors.py

---
+++ Lesson 10 - Prime and composite numbers/CountFactors.py
@@ -0,0 +1,13 @@
+def solution(N):
+    count = 0
+    i = 1
+    while i*i<N:
+        if N%i==0:
+            count += 2
+        i += 1
+    if i*i==N:
+        count += 1
+    return count
+    pass
+
+print(solution(24))
